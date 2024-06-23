cbd33a0e1bf6044d22bb1952f1e240edca9c8762
Orn-Lars <larasatioriana@gmail.com>
2024-06-23 15:39:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,4 +40,33 @@ async def joined(ctx, member: discord.Member):
 async def confidence(ctx):
     await ctx.send('Yes, the bot is cool.')
 
+@bot.command()
+async def meme(ctx):
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:
+        # Mari simpan file perpustakaan/library Discord yang dikonversi dalam variabel ini!
+        picture = discord.File(f)
+   # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command()
+async def cat(ctx):
+    cat_img = random.choice(os.listdir('catimg'))
+    with open(f'catimg/{cat_img}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
 bot.run(token)
